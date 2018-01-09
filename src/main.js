df96c8f2bcca0be112cece20c47fc06b22771270
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-09 10:15:48 -0200

    Add AlbumInfo to layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumsInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Nirvana');
 const albumList = document.getElementById('album-list');
 
-albums.then(data => renderAlbums(data.albums.items, albumList));
+const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+const albumInfo = document.getElementById('album-info');
+
+albums
+  .then(data => renderAlbums(data.albums.items, albumList));
+album
+  .then(data => renderAlbumsInfo(data, albumInfo));
