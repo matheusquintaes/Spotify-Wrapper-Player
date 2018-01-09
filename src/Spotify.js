53a40d290536f1d9960d8b7d294d4e276e72e737
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-09 09:54:46 -0200

    Add AlbumsList to interface

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper-js';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE',
+  token: 'BQCGYdl4ev-9Qvsi82r1hpmdKQQFWylY68bBIBbGbnKWUpoPcef-8iWgyx7Z0OvEyBCtv2M16dug-m3CRnNmCfklciSWufzJE4QRxAECRiZnu9W0K6CjNPA-EN-s1_iQDa-noWurw7fAFYg',
 });
 
 export default spotify;
