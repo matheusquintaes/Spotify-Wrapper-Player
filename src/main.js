ed26adefad4d1ac680c0e892144d8a7754af06de
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-10 14:42:28 -0200

    Add SelectAlbumTrigger

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
-import spotify from './Spotify';
-import renderAlbumInfo from './AlbumInfo';
-import renderAlbumTracks from './AlbumTracks';
 import searchEnterTrigger from './SearchTrigger';
+import selectAlbumTrigger from './SelectAlbumTrigger';
 
 searchEnterTrigger();
-
-const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
-const albumInfo = document.getElementById('album-info');
-
-const albumTracks = document.getElementById('album-tracks');
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
