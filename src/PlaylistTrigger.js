f7375648829ef4c3cef07e980213a657e0cd430d
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-10 14:51:08 -0200

    Add PlaylistTrigger

---
+++ src/PlaylistTrigger.js
@@ -0,0 +1,28 @@
+/* global Audio */
+const albumTracks = document.getElementById('album-tracks');
+let audioObject = null;
+
+export default function playlistTrigger() {
+  albumTracks.addEventListener('click', (e) => {
+    const target = e.target.parentNode;
+    console.log(target);
+    if (target.classList.contains('active')) {
+      audioObject.pause();
+    } else {
+      if (audioObject) {
+        audioObject.pause();
+      }
+      audioObject = new Audio(target.getAttribute('data-track-preview'));
+      audioObject.play();
+      target.classList.add('active');
+
+      audioObject.addEventListener('ended', () => {
+        target.classList.remove('active');
+      });
+
+      audioObject.addEventListener('pause', () => {
+        target.classList.remove('active');
+      });
+    }
+  });
+}
